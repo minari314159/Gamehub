4a8b72f0ae9d623e6c7ec557b13285a4474bb219
SJ <127341132+minari314159@users.noreply.github.com>
2023-06-22 13:40:21 -0400

    Display critic score colour scheme

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,21 +1,25 @@
-import { Card, CardBody, Image, Heading, Text } from "@chakra-ui/react"
-import { Games } from "../Hooks/useGames"
+import { Card, CardBody, Image, Heading, Text, HStack } from "@chakra-ui/react";
+import { Games } from "../Hooks/useGames";
 import Platform_Icon from "./Platform_Icon";
+import CriticScore from "./CriticScore";
 
 interface Props {
-    game: Games;
+  game: Games;
 }
 
-const GameCard = ({game}: Props) => {
+const GameCard = ({ game }: Props) => {
   return (
-    <Card borderRadius={10} overflow='hidden'>
-        <Image src={game.background_image}/>
-        <CardBody >
-            <Heading fontSize='2xl'>{game.name}</Heading>
-            <Platform_Icon platforms={game.parent_platforms.map(p => p.platform)} />
-        </CardBody>
+    <Card borderRadius={10} overflow="hidden">
+      <Image src={game.background_image} />
+      <CardBody justifyContent="space-between">
+        <Heading fontSize="2xl">{game.name}</Heading>
+        <HStack justifyContent="space-between">
+          <Platform_Icon platforms={game.parent_platforms.map((p) => p.platform)}/>
+          <CriticScore score={game.metacritic} />
+        </HStack>
+      </CardBody>
     </Card>
-  )
-}
+  );
+};
 
-export default GameCard
+export default GameCard;
