20899cb00e732f95f6aada8dc97c0f5a20d4d4de
SJ <127341132+minari314159@users.noreply.github.com>
2023-06-23 13:57:30 -0400

    Recreated colour theme

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -11,7 +11,7 @@ export interface Props {
 const GameGrid = ({gameQuery}: Props) => {
     const {data, error, isLoading } = useGames(gameQuery);
     const skeletons = [1,2,3,4,5,6];
-
+  
   return (
     <>
     {error && <Text>{error}</Text> }
