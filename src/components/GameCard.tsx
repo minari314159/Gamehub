13f3b86e73e24ec528de2be497f013fbba391e00
SJ <127341132+minari314159@users.noreply.github.com>
2023-06-23 13:31:58 -0400

    Rating emojis added

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -3,6 +3,7 @@ import { Games } from "../Hooks/useGames";
 import Platform_Icon from "./Platform_Icon";
 import CriticScore from "./CriticScore";
 import getCroppedImageUrl from "../services/imageurl";
+import Emoji from "./Emoji";
 
 
 interface Props {
@@ -14,11 +15,12 @@ const GameCard = ({ game }: Props) => {
     <Card >
       <Image src={getCroppedImageUrl (game.background_image)} />
       <CardBody justifyContent="space-between">
-        <Heading fontSize="2xl">{game.name}</Heading>
-        <HStack justifyContent="space-between">
+
+        <HStack justifyContent="space-between" marginBottom={3}>
           <Platform_Icon platforms={game.parent_platforms.map((p) => p.platform)}/>
           <CriticScore score={game.metacritic} />
         </HStack>
+        <Heading fontSize="2xl">{game.name}<Emoji rating={game.rating_top}/></Heading>
       </CardBody>
     </Card>
   );
