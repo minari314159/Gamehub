29ae2933834098ddbc7d02d0fe66758c20dafcb8
SJ <127341132+minari314159@users.noreply.github.com>
2023-06-26 18:53:30 -0400

    Fix the nav bar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,13 +2,16 @@ import { HStack, Image } from "@chakra-ui/react";
 import logo from '../assets/logo.png'
 import ThemeSwitch from "./ThemeSwitch";
 import SearchInput from "./SearchInput";
+import { Link } from "react-router-dom";
 
 
 
 const NavBar = () => {
   return (
    <HStack padding='15px'>
-    <Image src={logo} boxSize='60px'/>
+    <Link to='/'>
+     <Image src={logo} boxSize='60px' objectFit='cover'/>
+    </Link>
     <SearchInput/>
     <ThemeSwitch />
    </HStack>
